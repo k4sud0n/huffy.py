51b954cf5e8ce9d1fe70a8c9e388464c59d49229
Sihyun Kim <k4sud0n@gmail.com>
2024-12-22 12:39:03 +0900

    add router

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-from fastapi import FastAPI
+from datetime import datetime
+
+import aiohttp
+from fastapi import FastAPI, APIRouter
+from pyquery import PyQuery
 
 app = FastAPI()
+router = APIRouter(prefix="/api/v1", tags=["v1"])
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@router.get("/notice")
+async def read_notice(limit: int = 5):
+    return {"message": limit}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@router.get("/menu/{name}")
+async def read_menu(name: str):
+    # today_date = datetime.now().strftime("%Y%m%d")
+    #  url = f"https://wis.hufs.ac.kr/jsp/HUFS/cafeteria/viewWeek.jsp?startDt={today_date}&endDt={today_date}&caf_id=h101"
+    url = "https://wis.hufs.ac.kr/jsp/HUFS/cafeteria/viewWeek.jsp?startDt=20241219&endDt=20241219&caf_id=h205"
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            data = await response.text()
+
+    doc = PyQuery(data)
+
+    return {"data": doc("table").text()}
+
+
+app.include_router(router)
